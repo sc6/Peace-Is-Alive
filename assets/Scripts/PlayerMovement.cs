504b9b49e2d538df235cc5255a0db9a4c1f8af88
Steve Chang <steve.chang.1994@gmail.com>
2015-12-23 23:42:09 -0800

    Fixed mainchar movement animation: fine tuning

--- assets/Scripts/PlayerMovement.cs
+++ assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,9 @@ public class PlayerMovement : MonoBehaviour {
 	Animator anim;
     
     Vector3 pos, dir_up, dir_down, dir_left, dir_right, raycast_pos;                                
-    float speed = 2.0f;                         // Speed of movement
+    float speed = 4.7f;                         // speed of movement
 
-    // Use this for initialization
+
     void Start () {
 		anim = GetComponent<Animator> ();
 
@@ -20,24 +20,33 @@ public class PlayerMovement : MonoBehaviour {
         dir_right = transform.TransformDirection(Vector3.right);
     }
 	
-	// Update is called once per frame
+
 	void FixedUpdate () {
 
-        raycast_pos = transform.position;       //offset raycast to start from character's feet
+        //offset raycast to start from character's feet
+        raycast_pos = transform.position;       
         raycast_pos.x += 1f;
         raycast_pos.y -= 1.1f;
 
-        //Debug.DrawRay(raycast_pos, dir_down, Color.green);    //DEBUG: render raycast
+        //Debug.DrawRay(raycast_pos, dir_down, Color.green);    //debug: render raycast
 
-   
+        //player movement animation
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || 
+            Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || 
+            Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || 
+            Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)
+            )
+            anim.SetBool("isWalking", true);
+        else
+            if(transform.position == pos) anim.SetBool("isWalking", false);
+
+        //changes player position
         if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && transform.position == pos)
         {
-            if(transform.position == pos && !Physics2D.Raycast(raycast_pos, dir_left, 1))
-            {
-                pos += Vector3.left;
-                anim.SetBool("isWalking", true);
-            }
-            else anim.SetBool("isWalking", false);
+            if (transform.position == pos && !Physics2D.Raycast(raycast_pos, dir_left, 1))
+                pos += Vector3.left; 
+            else
+                anim.SetBool("isWalking", false);
 
             anim.SetFloat("input_x", -1.0f);
             anim.SetFloat("input_y", 0f);
@@ -45,11 +54,9 @@ public class PlayerMovement : MonoBehaviour {
         else if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && transform.position == pos)
         {
             if (transform.position == pos && !Physics2D.Raycast(raycast_pos, dir_right, 1))
-            {
                 pos += Vector3.right;
-                anim.SetBool("isWalking", true);
-            }
-            else anim.SetBool("isWalking", false);
+            else
+                anim.SetBool("isWalking", false);
 
             anim.SetFloat("input_x", 1.0f);
             anim.SetFloat("input_y", 0f);
@@ -57,11 +64,9 @@ public class PlayerMovement : MonoBehaviour {
         else if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && transform.position == pos)
         {
             if (transform.position == pos && !Physics2D.Raycast(raycast_pos, dir_up, 1))
-            {
                 pos += Vector3.up;
-                anim.SetBool("isWalking", true);
-            }
-            else anim.SetBool("isWalking", false);
+            else
+                anim.SetBool("isWalking", false);
 
             anim.SetFloat("input_x", 0f);
             anim.SetFloat("input_y", 1.0f);
@@ -69,21 +74,16 @@ public class PlayerMovement : MonoBehaviour {
         else if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && transform.position == pos)
         {
             if(!Physics2D.Raycast(raycast_pos, dir_down, 1))
-            {
                 pos += Vector3.down;
-                anim.SetBool("isWalking", true);
-
-            } else anim.SetBool("isWalking", false);
+            else
+                anim.SetBool("isWalking", false);
             
             anim.SetFloat("input_x", 0f);
             anim.SetFloat("input_y", -1.0f);
         }
-        else
-        {
-            anim.SetBool("isWalking", false);
-        }
 
-        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
+        //update position (commit)
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
 	}
 }
 
